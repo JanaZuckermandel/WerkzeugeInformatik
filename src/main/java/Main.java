bda12f2bc0de76b6ef43ff5e417cc5dbf7232cb2
Jana Zuckermandel <janazuckermandel@gmail.com>
2025-11-03 00:03:58 +0100

    Änderung in merge-conflict-Branche

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Car car2 = new Car("green", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
 	Car car6 = new Car("white", "Ford", 5);
+	Car car5 = new Car("white", "Ford", 5);
 
         Car car4 = new Car("yellow", "Renault", 4);
 
